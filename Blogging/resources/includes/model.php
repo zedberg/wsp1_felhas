e52778170f7f791b53ecbe5e43c4b9bdf50a56c9
zoidberg <42643022+zedberg@users.noreply.github.com>
2018-12-06 10:06:43 +0100

    First commit
    Forgot to commit before computer crashed. so had to start all over. THis is a new beginning.

---
+++ Blogging/resources/includes/model.php
@@ -0,0 +1,7 @@
+<?php
+
+//Simple array for topics - Övning 4
+
+//2D Associative array for full posts - Övning 9
+
+?>
---
+++ Blogging/resources/templates/single-blog-post-template.php
@@ -0,0 +1,14 @@
+<?php
+//Include Meta
+require ('resources/includes/head.php');
+
+//Include Header
+require ('resources/views/header.php');
+
+navigation($header);
+
+//Content
+
+//Inlcude Footer
+require ('resources/views/footer.php');
+?>
